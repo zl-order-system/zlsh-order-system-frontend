0cc3de1a3871d0e18cb909a11c6523b550f596e4
tucker <tucker961220@gmail.com>
2023-11-05 17:47:02 +0800

    order頁面撰寫

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// components
 import Home from './pages/Home'
 import Nav from './components/Nav'
+import Order from './pages/Order';
 
 function App() {
   return (
     <div className='flex flex-col w-[100dvw] h-[100dvh]'>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </Router>
+      {/* <Home/> */}
       <Nav/>
     </div>
   )

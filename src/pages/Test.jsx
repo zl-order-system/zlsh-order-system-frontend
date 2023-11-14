1ed4113afb2b1fc866565748cca23e255e2852cd
tucker <tucker961220@gmail.com>
2023-11-14 18:50:24 +0800

    未完成

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,16 +3,7 @@ import { useState,useEffect } from "react";
 import { fetchOrderData } from "../../API/API.js";
 
 function c(){
-  const [orderData,setOrderData] = useState(null) //取得使用者訂餐資料
-  useEffect(()=>{
-      fetchOrderData().then((value)=>{
-          setOrderData(value)
-      })
-  },[])
-  // useEffect(() => {    搞死人的異步
-  //   console.log(orderData);
-  // }, [orderData]);
-  return <E r={orderData}/>
+[1,2,3].map((item,index)=>{console.log(index);})
 }
 function E({r}){
   console.log(r)

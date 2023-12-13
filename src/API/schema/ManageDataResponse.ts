cf959f703736a111f06806381aa9873a07c21387
october <octoberserver@gmail.com>
2023-12-13 11:39:00 +0800

    changed schema

--- src/API/schema/ManageDataResponse.ts
+++ src/API/schema/ManageDataResponse.ts
@@ -6,19 +6,14 @@ type getManageDataResponse = {
         owed: number,
         daysUnordered: number,
     },
-    bodyData: [
-        {
-            state: OrderState,
-            date: Date,
-            displayDate: string,
-            id: string | undefined,
-            lunchBox: lunchBoxType | "-",
-            price: 65 | 70 | "-",
-            selectedMeal: string | "-",
-            mealOptions: [{
-                id: string,
-                name: string,
-            }]
-        }
-    ]
+    bodyData: {
+        state: OrderState,
+        date: Date,
+        displayDate: string,
+        id: string | undefined,
+        lunchBox: lunchBoxType | "-",
+        price: "65" | "70" | "-",
+        selectedMeal: string | "-",
+        mealOptions: string[]
+    }[]
 }

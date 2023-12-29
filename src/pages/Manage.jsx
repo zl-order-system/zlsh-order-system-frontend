879a00deb465c6a137ecd86fe0d2930612e695d6
tucker <tucker961220@gmail.com>
2023-12-29 20:43:39 +0800

    delete some annotation

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -96,27 +96,6 @@ function Manage() {
                         <div className="text-black text-[1.2rem] font-[700]">未訂: {data["headerData"]["daysUnordered"]}天</div>
                     </div>
                     <PreviewBar itemsData={data["bodyData"]} />
-                    {/* <div>
-                        <div className="h-[2rem] w-full bg-[#D0CFCF] rounded-[1.15rem]"></div>
-                        <div className="text-[#565656] text-[0.8rem] font-[400] flex flex-row justify-between my-[0.5rem]">
-                            <div>8/29</div>
-                            <div>8/30</div>
-                        </div>
-                        <div className="flex flex-row justify-start gap-[1.3rem] my-[1.25rem]">
-                            <div className="flex flex-row items-center">
-                                <div className="w-[0.8rem] h-[0.8rem] rounded-[50%] bg-[#97D581] mr-[0.3rem]"></div>
-                                <div className="text-[#565656] text-[0.8rem] font-[400]">已繳費</div>
-                            </div>
-                            <div className="flex flex-row items-center">
-                                <div className="w-[0.8rem] h-[0.8rem] rounded-[50%] bg-[#E2473D] mr-[0.3rem]"></div>
-                                <div className="text-[#565656] text-[0.8rem] font-[400]">未繳費</div>
-                            </div>
-                            <div className="flex flex-row items-center">
-                                <div className="w-[0.8rem] h-[0.8rem] rounded-[50%] bg-[#D0CFCF] mr-[0.3rem]"></div>
-                                <div className="text-[#565656] text-[0.8rem] font-[400]">未訂餐</div>
-                            </div>
-                        </div>
-                    </div> */}
                     <div className=" w-full h-[1px] bg-[#B6B6B6]"></div>
                     <div className=" flex flex-col items-center w-full">
                         <Items itemsData={data["bodyData"]} />

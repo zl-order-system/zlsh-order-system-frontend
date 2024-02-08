378c3e7794d2570e1115221059cff72bfabbd356
tucker <tucker961220@gmail.com>
2024-02-08 11:43:50 +0800

    .

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import "../loader/loader.css"
 function Loader({ state }){
     if (state == "close") return null
     return (
-        <div className="absolute left-[50%] top-[50%] block translate-x-[-50%] translate-y-[-50%]">
+        <div className="absolute left-[50%] top-[50%] block translate-x-[-50%] translate-y-[-50%] z-50">
             <div className="loader2"></div>
         </div>
     )

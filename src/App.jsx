ff31efd9e6adb40d1b7a5658527a6bbcf1d57484
Tucker <Tucker961220@gmail.com>
2024-02-13 19:58:11 +0800

    權限API修改

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import { setToken } from './utils/token';
 
 
 function App() {
-  const [hash, setHash] = useState(window.location.hash)
-  window.addEventListener('hashchange', () => {setHash(window.location.hash)})
+  const [hash, setHash] = useState(window.location.hash.split("?")[0])
+  window.addEventListener('hashchange', () => {setHash(window.location.hash.split("?")[0])})
   return (
     <div className='grid grid-cols-1 grid-rows-[1fr_4.3rem] w-[100dvw] h-[100dvh] min-w-[260px]'>
       <Router>
@@ -41,6 +41,8 @@ function TokenManager() {
 
   useEffect(() => {
     const token = searchParams.get("token");
+    // console.log('#/login'.split("?")[0]);
+    // console.log(window.location.hash)
     if (token == null || token == "") return;
     setToken(token)
     navigate("/");

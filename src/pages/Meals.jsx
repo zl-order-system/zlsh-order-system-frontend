1a5bad593ce2771df50b28224adb0482c3a7a10a
tucker <tucker961220@gmail.com>
2024-02-15 16:01:15 +0800

    .

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -34,7 +34,7 @@ function Hint(){
             <img src={yellowHintIcon} className="w-[1.4rem] inline mr-[0.5rem] mt-[-5px]" />
             <span className="text-[#1F2328] text-[1.1rem] font-[500]">
                 本頁菜單均為系統自動生成，僅供參考 。請以
-                <a className="text-[#0969DA] underline" href="https://www.zlsh.tp.edu.tw/category/office/div_300/section_lunch/lunch1_list/">學校公布的菜單</a>
+                <a className="text-[#0969DA] underline" href="https://www.zlsh.tp.edu.tw/category/office/div_300/section_lunch/lunch1_list/" target="_blank">學校公布的菜單</a>
                 為準
             </span>
         </div>

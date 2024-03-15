e56cae4f275ff5da2f720fc7194175acf733f6eb
october <octoberserver@gmail.com>
2024-03-15 15:48:04 +0800

    .

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import "../loader/Loader.css"
+function Loader(){
+  return (
+    <div className="absolute left-[50%] top-[50%] block translate-x-[-50%] translate-y-[-50%] z-50">
+      <div className="loader2"></div>
+    </div>
+  )
+}
+export default Loader

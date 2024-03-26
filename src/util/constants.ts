4cc812576614c6c612230953c666eb2baaeb7eb3
tucker <tucker961220@gmail.com>
2024-03-26 18:05:06 +0800

    add env

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,3 +2,17 @@ export const AppConstants = {
   MAIN_APP_URL: "https://zl-order-system.github.io/staging/app",
   BACKEND_HOST: "staging.order-system.octoberserver.net"
 }
+
+export function getAppConstants() {
+  let MODE = process.env.MODE
+    if (MODE === undefined || MODE === "staging")
+        return {
+          MAIN_APP_URL: "https://zl-order-system.github.io/staging/app",
+          BACKEND_HOST: "staging.order-system.octoberserver.net"
+        }
+    else
+        return {
+          MAIN_APP_URL: "https://zl-order-system.github.io/app",
+          BACKEND_HOST: "order-system.octoberserver.net"
+        }
+}

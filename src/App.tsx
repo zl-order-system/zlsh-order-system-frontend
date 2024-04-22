10a41e306d7498816fab171e9da0ab4bddffa8d5
tucker <tucker961220@gmail.com>
2024-04-22 11:18:26 +0800

    add staging hint

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,8 @@ function Main() {
   const {pathname} = useLocation();
 
   return (
+    <>
+    {process.env.MODE === "staging" && <div className='bg-[#5e5e5e] fixed bottom-3 right-3 px-3 opacity-40 text-2xl rounded-md'>staging</div>}
     <div className='grid grid-cols-1 grid-rows-[1fr_4.3rem] w-[100dvw] h-[100dvh] min-w-[260px]'>
       <TokenManager/>
       <Routes>
@@ -39,6 +41,7 @@ function Main() {
       </Routes>
       {navCheck(pathname) && <Nav/>}
     </div>
+    </>
   )
 }
 

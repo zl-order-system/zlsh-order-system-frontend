502a7bbbaa07120296f7ae9e42810bae514c5bc5
tucker <tucker961220@gmail.com>
2024-05-26 13:27:21 +0800

    update meals page

--- src/pages/Meals/Meals.tsx
+++ src/pages/Meals/Meals.tsx
@@ -1,13 +1,21 @@
 import { Hint } from "./Hint"
-import { createFakeData } from "./fakeData"
 import { GetMealsData } from "../../API/schema/meals"
 import { MealsBlocks } from "./MealsBlocks"
 import { getDate } from "../../util/util"
+import { useQuery } from "react-query"
+import Loader from "../../components/Loader/Loader"
 
 export function Meals() {
   // 目前API網址：https://zl-order-system.github.io/zlsh-order-system-crawl/API/meals/latest.json
-  const mealsData = JSON.parse(createFakeData()) as GetMealsData
+  const {data, isError} = useQuery(["fetchMeals"], fetchMealsData)
+  const mealsData = data as GetMealsData
 
+  if (mealsData === undefined) return <Loader/>
+  isError && (
+    <div className="flex justify-center items-center">
+      <div className="text-red-600 text-[1.8rem] font-[700]">資料獲取失敗</div>
+    </div>
+  )
   return (
     <div className="flex pt-[0.5rem] px-[1.2rem] gap-[0.7rem] flex-col overflow-scroll">
       <div className="text-black text-[1.8rem] font-[700]">查看菜單</div>
@@ -20,4 +28,9 @@ export function Meals() {
 function getTitleMonth(mealsData: GetMealsData){
   let len = Math.floor(mealsData.length / 2)
   return  getDate(new Date(mealsData[len].date)).month + "月"
+}
+async function fetchMealsData(){
+  const mealsUrl = "https://zl-order-system.github.io/zlsh-order-system-crawl/API/meals/latest.json"
+  let res = await fetch(mealsUrl)
+  return  res.json()
 }

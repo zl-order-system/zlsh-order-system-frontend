a88508cf04394a63b2277211d5970f35f53c01b6
october <71247836+october1234@users.noreply.github.com>
2025-04-18 09:05:30 +0800

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -4,13 +4,13 @@ export function getAppConstants() {
     if (MODE === undefined || MODE === "staging")
         return {
           MAIN_APP_URL: "https://zl-order-system.github.io/staging/app",
-          BACKEND_HOST: "staging.order-system.octoberserver.net",
+          BACKEND_HOST: "staging.order-system.octsrv.org",
           MANAGEMENT_URL: "https://zl-order-system.github.io/staging/management/#/"
         }
     else
         return {
           MAIN_APP_URL: "https://zl-order-system.github.io/app",
-          BACKEND_HOST: "order-system.octoberserver.net",
+          BACKEND_HOST: "order-system.octsrv.org",
           MANAGEMENT_URL: "https://zl-order-system.github.io/management/#/"
         }
 }
